13788cf2dd50c550a54973eb3b5e70c5b17f2869
Hsing Lo <hsing@flyingbat.net>
2022-04-29 22:40:44 -0400

    simple grid fix extra value tested and fp problems

--- simple-grid.c
+++ simple-grid.c
@@ -1,22 +1,24 @@
 #include "mandelbrot-area.h"
-#define GRID_SIZE 1000
+#define GRID_SIZE 8192
 
 int main() {
-    prnginit();    
     reseed();
+    prnginit();    
 
     unsigned long gridTested = 0;
-    const double delta = 2.49 / GRID_SIZE;
+    const double deltaRe = 2.49 / GRID_SIZE;
+    const double deltaIm = 1.15 / GRID_SIZE;
 
     while (1) {
         unsigned long startTime = time(NULL);
         uint64_t member = 0, notmem = 0, undeci = 0, tested = 0;
+        double wIm, wRe;
 
-        for (double wIm = 0; wIm < 1.15; wIm += delta) {
-            for (double wRe = -2.0; wRe < 0.49; wRe += delta) {
+        for (wIm = 0; wIm < 1.15 - deltaIm / 2; wIm += deltaIm) {
+            for (wRe = -2.0; wRe < 0.49 - deltaRe / 2; wRe += deltaRe) {
                 char memdat = membership(
-                    wRe + _01() * delta,
-                    wIm + _01() * delta);
+                    wRe + _01() * deltaRe,
+                    wIm + _01() * deltaIm);
                 member += memdat == MEMBER;
                 notmem += memdat == NOT_A_MEMBER;
                 undeci += memdat == UNDECIDED;
