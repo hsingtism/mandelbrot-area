6398650e0f14a09a79f24baff1071137815c0c23
Hsing Lo <hsing@flyingbat.net>
2022-05-02 00:03:53 -0400

    less comparison, removed unnessary ones

--- mem-func.c
+++ mem-func.c
@@ -33,8 +33,7 @@ double _01() {
 }
 
 void reseed() {
-    uint64_t urs0;
-    uint64_t urs1;
+    uint64_t urs0, urs1;
 
     if (RNGSEED) {
         FILE *randsource;
@@ -53,43 +52,34 @@ void reseed() {
 }
 
 char membership(double re, double im) {
-    //test bonds
+    // test bonds
     if (im < -1.15 || im > 1.15) return NOT_A_MEMBER;
     if (re < -2.0 || re > 0.49) return NOT_A_MEMBER;
     if (re * re + im * im > 4.0) return NOT_A_MEMBER;
 
     const double cRe = re, cIm = im;
-    double inRe, inIm, pRe, pIm, pobRe;
+    double pRe, pIm, pobRe;
     double obRe = re, obIm = im;
 
     for (unsigned long i = 0; i < dwellLimit; i++) {
-        if (re * re + im * im > 4.0) return NOT_A_MEMBER; // out of bonds
-        if (re != re || im != im) return NOT_A_MEMBER;    // NaN
-        if (pRe == re && pIm == im) return MEMBER;        // convergence
-
         pRe = re;
         pIm = im;
         // iterate
         re = (re + im) * (re - im) + cRe;
         im = 2 * pRe * im + cIm;
 
-        if (cRe == re && cIm == im) return MEMBER;  // cyclic
+        if (i % 2) {
+            if (re * re + im * im > 4.0) return NOT_A_MEMBER;  // out of bonds
+            if (pRe == re && pIm == im) return MEMBER;         // convergence
 
-        if (i % 2) {  // orbit detection
+            // orbit detection
             pobRe = obRe;
             obRe = (obRe + obIm) * (obRe - obIm) + cRe;
             obIm = 2 * pobRe * obIm + cIm;
             if (obRe == re && obIm == im) return MEMBER;
         }
     }
+
     printf("UNDECIDED re: %lf im: %lf - %016llx %016llx\n", cRe, cIm, *(uint64_t *)&cRe, *(uint64_t *)&cIm);
-
-    if (FILE_OUTPUT == 1) {
-        FILE *fp;
-        fp = fopen("undecided.txt", "a");
-        fprintf(fp, "UNDECIDED re: %lf im: %lf - %016llx %016llx\n", cRe, cIm, *(uint64_t *)&cRe, *(uint64_t *)&cIm);
-        fclose(fp);
-    }
-
     return UNDECIDED;
 }
